50120d5323350543ab0024c91c33f492e81de057
armandhuang <armand.huang@gmail.com>
2019-02-19 14:11:06 +0800

    Java and kotlin varable, data type

--- src/main/java/com/jason/Hello.java
+++ src/main/java/com/jason/Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 //        System.out.println("Hello world");
         Person p = new Person();
         p.hllo();
+        int age = 19;
+        Integer age2 = 19;
+        char c ="我";
+        Character c2 = 'A';
+        byte b =120;
+        Byte b2 = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
     }
 }
